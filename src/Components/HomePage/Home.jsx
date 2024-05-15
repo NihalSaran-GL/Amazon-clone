4032ccdd70715a2ea5e08a0f8773989845b97ed9
sajalsat <sajal.satsangi@globallogic.com>
2024-05-16 00:07:04 +0530

    Product Detalis Page Created

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import MyCard from './Card/Card';
 import Imagecarousel from './ImageCarousel/Imagecarousel';
 import './Home.css';
-import ImageCarousel from './ImageCarousel/Imagecarousel1';
+import { Slider1 } from './Slider1/slider1';
+import { Slider2 } from './Slider2/slider2'
+import { Slider3 } from './Slider3/slider3'
 import { Footer } from '../Footer/Footer';
 import { FooterBottom } from '../Footer/FooterBottom/FooterBottom';
-import { Slider1 } from './Slider1/slider1';
-import {Slider2} from './Slider2/slider2'
-import {Slider3} from './Slider3/slider3'
-import {FooterSignIn} from '../Footer/FooterSignIn/FooterSignIn'
+import Navbottom from '../Navbar/navbottom/navbottom';
+import Navbar from '../Navbar/Navbar';
+import { FooterSignIn } from '../Footer/FooterSignIn/FooterSignIn'
 
 
 function Home() {
   return (
     <div>
+      <Navbar />
+      <Navbottom />
       <Imagecarousel />
       <div className="card-grid">
         <MyCard title="Another Card" content="This is another card." />
@@ -22,10 +25,10 @@ function Home() {
       <Slider2 />
       <Slider3 />
       <FooterSignIn />
-      <Footer/>
-      <FooterBottom/>
+      <Footer />
+      <FooterBottom />
     </div>
   );
-} 
+}
 
 export default Home;
