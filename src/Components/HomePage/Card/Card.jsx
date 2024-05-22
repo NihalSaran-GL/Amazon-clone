6977de745be35572a9b510c1d45df488f3a3c01d
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-05-22 15:17:29 +0530

    Dynamically adjusted

--- src/Components/HomePage/Card/Card.jsx
+++ src/Components/HomePage/Card/Card.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './Card.css';
 import { Card, CardContent, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const MyCard = () => {
   const [cards, setCards] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch('https://vbcm7hql-3001.inc1.devtunnels.ms/cards')
       .then(response => response.json())
       .then(data => setCards(data));
   }, []);
+
+  // Function to handle image click and navigate to product page
+  const handleImageClick = (imageName) => {
+    navigate(`/product/${imageName}`);
+  };
 
   return (
     <div className="card-container">
@@ -21,7 +28,7 @@ const MyCard = () => {
             </Typography>
             <div className='cardImageContainer'>
               {card.images.slice(0, 2).map((image, i) => (
-                <div key={i} className="cardImage">
+                <div key={i} className="cardImage" onClick={() => handleImageClick(image.src)}>
                   <img className='card-img' src={`/Amazon-clone/assets/${image.src}`} alt="" />
                   <span className='card-img-des'>{image.description}</span>
                 </div>
@@ -29,7 +36,7 @@ const MyCard = () => {
             </div>
             <div className='cardImageContainer'>
               {card.images.slice(2, 4).map((image, i) => (
-                <div key={i} className="cardImage">
+                <div key={i} className="cardImage" onClick={() => handleImageClick(image.src)}>
                   <img className='card-img' src={`/Amazon-clone/assets/${image.src}`} alt="" />
                   <span className='card-img-des'>{image.description}</span>
                 </div>
