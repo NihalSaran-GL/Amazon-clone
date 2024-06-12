c5276efe003f340bd715ba244b1994912f0a7ffd
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-06-12 13:15:38 +0530

    update mainHeader

--- src/Components/HeaderContainer/MainHeader/MainHeader.jsx
+++ src/Components/HeaderContainer/MainHeader/MainHeader.jsx
@@ -4,11 +4,15 @@ import Logo from "../../ReusableComponets/Logo";
 import { useLogoData} from "../../ReusableComponets/Hooks";
 import englishData from "../../../../public/data/English.json";
 import imagesData from "../../../../public/data/Image.json";
+import iconData from "../../../../public/data/Icon.json";
+import Icons from "../../ReusableComponets/Icons";
+import { LocationOn } from "@mui/icons-material";
 
 const LogoData = {
   image: imagesData.amazon_logo_image,
   text: englishData.amazon_logo_text,
   logoName: englishData.logo_name,
+  iconName: iconData.location,
 };
 
 function Main_Header() {
@@ -25,6 +29,7 @@ function Main_Header() {
           width={LogoWidth}
           height={LogoHeight}
         />
+        <Icons Icon={LocationOn} iconColor="white" iconSize="1.5rem" />
       </LeftSection>
       <CenterSection>CenterSection</CenterSection>
       <RightSection>RightSection</RightSection>
--- src/Components/ReusableComponets/Colors.jsx
+++ src/Components/ReusableComponets/Colors.jsx
@@ -1,7 +1,7 @@
 const colors = {
   primary: "#3498db",
   secondary: "#2ecc71",
-  background: "#ecf0f1",
+  mainHeaderBackground: "rgb(19, 25, 33)",
   text: "#2c3e50",
   white: "#ffffff",
   black: "#000000",
