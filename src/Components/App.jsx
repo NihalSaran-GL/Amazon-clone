5a1be8850845f129c2b54b169b9bfe5150d4fac2
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-07-08 17:00:30 +0530

    update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../Screens/Home";
 import Header from "./HeaderContainer/HeaderContainer";
+import ProductCategories from "../Screens/ProductCategories";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/AmazonFashion" element={<ProductCategories />} />
       </Routes>
     </Router>
   );
