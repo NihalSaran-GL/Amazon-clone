d959c9ca5a37f5f49279cf32b26ce497aae099b2
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-07-31 16:14:56 +0530

    update the slider

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -22,47 +22,12 @@ const ImageWrapper = styled.div`
   }
 `;
 
-const StyledCard = styled.article`
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 10px;
-  text-align: center;
-  background-color: #f8f9fa;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  height: 150px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin: 0 5px;
-`;
-
-const CardTitle = styled.h2`
-  color: #333;
-  margin-bottom: 5px;
-  font-size: 1rem;
-`;
-
-const CardContent = styled.p`
-  color: #666;
-  font-size: 0.8rem;
-`;
-
-
-
 const imagess = [
   'assets/productimg.jpg',
   'assets/productimg2.jpg',
   'assets/productimg3.jpg',
   'assets/productimg4.jpg',
-  // Add more image URLs here
 ];
-
-const Cardd = ({ title, content }) => (
-  <StyledCard>
-    <CardTitle>{title}</CardTitle>
-    <CardContent>{content}</CardContent>
-  </StyledCard>
-);
 
 const images = "https://cdn.pixabay.com/photo/2022/01/25/04/42/bird-6965228_1280.jpg "
     .repeat(10)
@@ -78,8 +43,35 @@ function Home() {
     setCategories(categoriesData.CategoryCard);
   }, []);
 
-  const cards = Array.from({ length: 20 }, (_, i) => (
-    <Cardd key={i + 1} title={`Card ${i + 1}`} content={`Content ${i + 1}`} />
+  const cards = Object.entries(categories).map(([key, category]) => (
+    <Card
+      key={key}
+      title={category.title}
+      cardsPerRow={2}
+      content={
+        <>
+          {Object.values(category).map((cat) =>
+            typeof cat === "object" ? (
+              <ImageWrapper key={cat.id}>
+                <img src={cat.image} alt={cat.name} />
+                <p>{cat.name}</p>
+              </ImageWrapper>
+            ) : null
+          )}
+        </>
+      }
+      footer={category.footer}
+      headerFontSize={textSize.M}
+      headerMarginBottom={size.S}
+      contentFontSize={textSize.XS}
+      contentMarginBottom={size.M}
+      footerFontSize={textSize.XXS}
+      footerColor={colors.octonary}
+      footerTextAlign="left"
+      padding={size.M}
+      margin={size.XS}
+      wrapperBgColor={colors.primary}
+    />
   ));
 
 
@@ -99,36 +91,7 @@ function Home() {
       }}
     />
       <GridWrapper columns={"4"}>
-        {Object.entries(categories).map(([key, category]) => (
-          <Card
-          key={key}
-          title={category.title}
-          cardsPerRow={2}
-          content={
-            <>
-              {Object.values(category).map((cat) =>
-                typeof cat === "object" ? (
-                  <ImageWrapper key={cat.id}>
-                    <img src={cat.image} alt={cat.name} />
-                    <p>{cat.name}</p>
-                  </ImageWrapper>
-                ) : null
-              )}
-            </>
-            }
-            footer={category.footer}
-            headerFontSize={textSize.M}
-            headerMarginBottom={size.S}
-            contentFontSize={textSize.XS}
-            contentMarginBottom={size.M}
-            footerFontSize={textSize.XXS}
-            footerColor={colors.octonary}
-            footerTextAlign="left"
-            padding={size.M}
-            margin={size.XS}
-            wrapperBgColor={colors.primary}
-          />
-        ))}
+        {cards}
       </GridWrapper>
       <CardSlider cards={cards} />
     </Container>
