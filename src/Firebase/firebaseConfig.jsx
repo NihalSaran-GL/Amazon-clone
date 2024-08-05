86a68d5bac9c1b2a3e613fe351375a32ed32687c
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-08-06 00:55:29 +0530

    .env file integrated

--- src/Firebase/firebaseConfig.jsx
+++ src/Firebase/firebaseConfig.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_iM8eRcYObPit6c-LSaLzB2-mAnCajp4",
-  authDomain: "clone-f1ccd.firebaseapp.com",
-  projectId: "clone-f1ccd",
-  storageBucket: "clone-f1ccd.appspot.com",
-  messagingSenderId: "167467505553",
-  appId: "1:167467505553:web:39ce8b8729c73d88152c30",
-  measurementId: "G-2DF81P6CKL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
